dc3dd290bdd98e841c221e56c368fd21f45ef3b8
nuhcolakkadioglu <hex-tr@hotmail.com>
2021-09-19 14:43:08 +0300

    libmana geçiş yapıldı.

--- StokTakibi.Mvc/Startup.cs
+++ StokTakibi.Mvc/Startup.cs
@@ -34,13 +34,14 @@ namespace StokTakibi.Mvc
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseStatusCodePages();
             }
             app.UseStaticFiles();
 
-            app.UseStaticFiles(new StaticFileOptions() {
-                FileProvider = new PhysicalFileProvider(
-                    Path.Combine(Directory.GetCurrentDirectory(), "node_modules")),RequestPath = new PathString("/node_modules")
-            });
+            //app.UseStaticFiles(new StaticFileOptions() {
+            //    FileProvider = new PhysicalFileProvider(
+            //        Path.Combine(Directory.GetCurrentDirectory(), "node_modules")),RequestPath = new PathString("/node_modules")
+            //});
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
