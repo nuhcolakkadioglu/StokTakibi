3de1948f3a3da7f6afe05c9ab52ec1ef534e395e
nuhcolakkadioglu <hex-tr@hotmail.com>
2021-10-07 13:18:15 +0300

    .

--- StokTakibi.Mvc/Models/Product.cs
+++ StokTakibi.Mvc/Models/Product.cs
@@ -8,5 +8,7 @@ namespace StokTakibi.Mvc.Models
     public class Product
     {
         public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
     }
 }
